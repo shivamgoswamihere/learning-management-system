d53b8a3ee3a392da27dbc4baa4cd15fe7e1e5410
Krishna-2115 <krishnatyagi892@gmail.com>
2025-03-08 23:32:58 +0530

    update

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,29 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUserById } from "../redux/userSlice";
-import { useParams } from "react-router-dom";
+import React from 'react'
 
-const UserProfile = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    const { user, loading, error } = useSelector((state) => state.users);
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
 
-    useEffect(() => {
-        dispatch(fetchUserById(id));
-    }, [dispatch, id]);
-
-    if (loading) return <p>Loading user...</p>;
-    if (error) return <p>Error: {error}</p>;
-
-    return user ? (
-        <div>
-            <h2>{user.fullName}</h2>
-            <p>Email: {user.email}</p>
-            <p>Role: {user.role}</p>
-        </div>
-    ) : (
-        <p>No user found</p>
-    );
-};
-
-export default UserProfile;
+export default Profile
